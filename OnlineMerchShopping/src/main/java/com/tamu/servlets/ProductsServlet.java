087736cbce4b56055a695a4ed7c3f38e15a0f6cc
atharvagasheTAMU <atharvagashe22@tamu.edu>
2023-09-30 12:31:38 -0500

    Added files

---
+++ OnlineMerchShopping/src/main/java/com/tamu/servlets/ProductsServlet.java
@@ -0,0 +1,69 @@
+package com.tamu.servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.tamu.entity.Product;
+import com.tamu.dao.ApplicationDao;
+
+@WebServlet("/addProducts")
+public class ProductsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		//get the HTTPSession object
+		HttpSession session = request.getSession();
+		
+		//create a cart as arraylist for the user
+		List<String> cart = (ArrayList<String>)session.getAttribute("cart");
+		
+		if(cart==null){
+			cart = new ArrayList<>();
+		}
+
+		//add the selected product to the cart
+		if(request.getParameter("product")!=null){
+			cart.add(request.getParameter("product"));
+		}
+		
+		session.setAttribute("cart", cart);
+
+
+		//get search criteria from search servlet
+		
+		String search = (String)session.getAttribute("search");
+		
+		
+		//get the search results from dao
+		ApplicationDao dao = new ApplicationDao();
+		List<Product> products = null;
+		try {
+			products = dao.searchProducts(search);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//set the search results in request scope
+		request.setAttribute("products", products);
+		
+		//forward to searchResults.jsp
+		request.getRequestDispatcher("/html/searchResults.jsp").forward(request, response);
+
+
+	}
+
+
+	
+}
---
+++ OnlineMerchShopping/src/main/java/com/tamu/servlets/RegisterUserServlet.java
@@ -0,0 +1,89 @@
+package com.tamu.servlets;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.MessageFormat;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.tamu.entity.User;
+import com.tamu.dao.ApplicationDao;
+
+@WebServlet("/registerUser")
+public class RegisterUserServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		// collect all form data
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		String firstName = req.getParameter("fname");
+		String lastName = req.getParameter("lname");
+		String activity = req.getParameter("activity");
+		int age = Integer.parseInt(req.getParameter("age"));
+		
+
+		// fill it up in a User bean
+		User user = new User(username, password, firstName, lastName, age, activity);
+		
+		
+
+		// call DAO layer and save the user object to DB
+		ApplicationDao dao = new ApplicationDao();
+		int rows = 0;
+		try {
+			rows = dao.registerUser(user);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+		// prepare an information message for user about the success or failure of the operation
+		String infoMessage = null;
+		if(rows==0){
+			infoMessage="Sorry, an error occurred!";
+		}
+		else{
+			infoMessage="User registered successfully!";
+		}
+
+		// write the message back to the page in client browser\
+		String page = getHTMLString(req.getServletContext().getRealPath("/html/register.html"), infoMessage);
+		resp.getWriter().write(page);
+				
+				
+	}
+	
+	public String getHTMLString(String filePath, String message) throws IOException{
+		BufferedReader reader = new BufferedReader(new FileReader(filePath));
+		String line="";
+		StringBuffer buffer = new StringBuffer();
+		while((line=reader.readLine())!=null){
+			buffer.append(line);
+		}
+		
+		reader.close();
+		String page = buffer.toString();
+		
+		page = MessageFormat.format(page, message);
+		
+		return page;
+		
+		
+	}
+		
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String page = getHTMLString(req.getServletContext().getRealPath("/html/register.html"), "");
+		resp.getWriter().write(page);
+	}
+
+	
+}
